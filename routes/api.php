03072efe73794503b2f58d34d11f152d4f6b5e1c
edipl-projects <116559286+edipl-projects@users.noreply.github.com>
2023-04-12 13:46:38 +0200

    #PP-62 Dodanie routingów API dla operacji CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MountainGroupController;
+use App\Http\Controllers\TerrainPointController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'terrain-points'], function () {
+    Route::get('/', [TerrainPointController::class, 'index']);
+    Route::post('/', [TerrainPointController::class, 'store']);
+    Route::get('/{terrainPoint}', [TerrainPointController::class, 'show']);
+    Route::put('/{terrainPoint}', [TerrainPointController::class, 'update']);
+    Route::delete('/{terrainPoint}', [TerrainPointController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'mountain-groups'], function () {
+    Route::get('/', [MountainGroupController::class, 'index']);
+    Route::post('/', [MountainGroupController::class, 'store']);
+    Route::get('/{mountainGroup}', [MountainGroupController::class, 'show']);
+    Route::put('/{mountainGroup}', [MountainGroupController::class, 'update']);
+    Route::delete('/{mountainGroup}', [MountainGroupController::class, 'destroy']);
 });
