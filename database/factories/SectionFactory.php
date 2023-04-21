1769aca36196889b58d0d57de5388e8e6d8d5a09
edipl-projects <116559286+edipl-projects@users.noreply.github.com>
2023-04-21 21:51:38 +0200

    #PP-64 Poprawka fabryki

--- database/factories/SectionFactory.php
+++ database/factories/SectionFactory.php
@@ -16,11 +16,11 @@ class SectionFactory extends Factory
         return [
             'name' => $this->faker->name,
             'description' => $this->faker->sentence(10),
-            'mountain_range' => MountainRange::factory()->create(),
+            'mountain_range_id' => $this->faker->numberBetween(1,1),
             'badge_points_a_to_b' => $this->faker->numberBetween(1, 10),
             'badge_points_b_to_a' => $this->faker->numberBetween(1, 10),
-            'terrain_point_a' => TerrainPoint::factory()->create(),
-            'terrain_point_b' => TerrainPoint::factory()->create(),
+            'terrain_point_a_id' => $this->faker->numberBetween(1, 42),
+            'terrain_point_b_id' => $this->faker->numberBetween(1, 42),
         ];
     }
 }
